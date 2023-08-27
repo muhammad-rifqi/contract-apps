fe734cff2948bf1a16e5bcdbbb7129671bdbbcbf
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2023-08-27 22:34:12 +0700

    aaaa

--- index.php
+++ index.php
@@ -12,9 +12,13 @@
     <title>eKontrak - Kabupaten Sleman</title>
     <link href="css/public-app.css" rel="stylesheet">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    <link href="admin/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="admin/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    
+    
 
 </head>
-
 <body id="public-5" class="app">
     <div class="background-wrapper w-100"></div>
     <div class="p-0" style="min-height: 100vh;">
@@ -36,7 +40,7 @@
                     <div id="ig-tahun" class="input-group input-group-sm input-group">
                         <div class="input-group-prepend">
                             <div class="input-group-text bg-primary font-weight-bold"
-                                style="background-color: rgb(10 122 127) !important;">Tahun</div>
+                                style="background-color: rgb(10 122 127) !important; color: white">Tahun</div>
                         </div> <select class="form-control form-control-sm" name="tahun">
                             <option value="2023">2023</option>
                         </select>
@@ -51,21 +55,231 @@
             </div>
         </nav>
 
-    
         <div class="p-0 d-flex flex-column flex-wrap">
             <div id="content"> 
+                <br>
+                <div class="container container-bigger bg-white">
+        
+                        <div class="row">
+                                <div class="col-4 px-1">
+                                    <div class="card shadow mb-4">
+                                            <div class="card-header py-3">
+                                                <h6 class="m-0 font-weight-bold text-primary">Data Terbaru</h6>
+                                            </div>
+                                                <div class="card-body">
+                                                    
+                                                <div class="container-bigger bg-white">
+                                                    <div class="row">
+                                                        <div class="col">
+                                                                <div class="card shadow mb-4">
+                                                                    <div class="card-header py-3">
+                                                                    <h6 class="m-0 font-weight-bold text-success">Penyedia</h6>
+                                                                    </div>
+                                                                    <div class="card-body">
+                                                                        sdfsdfsdf
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+                                                        <div class="col">
+                                                                <div class="card shadow mb-4">
+                                                                    <div class="card-header py-3">
+                                                                    <h6 class="m-0 font-weight-bold text-success">Charts</h6>
+                                                                    </div>
+                                                                    <div class="card-body">
+                                                                        sdfsdfsdf
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+                                                        <div class="w-100"></div>
+                                                        <div class="col">
+                                                                <div class="card shadow mb-4">
+                                                                    <div class="card-header py-3">
+                                                                    <h6 class="m-0 font-weight-bold text-success">Kontrak</h6>
+                                                                    </div>
+                                                                    <div class="card-body">
+                                                                        sdfsdfsdf
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+                                                        <div class="col">
+                                                                <div class="card shadow mb-4">
+                                                                    <div class="card-header py-3">
+                                                                    <h6 class="m-0 font-weight-bold text-success"> Description </h6>
+                                                                    </div>
+                                                                    <div class="card-body">
+                                                                        sdfsdfsdf
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                    </div>
+                                </div>
 
-            <div class="container container-bigger bg-white" style="height: 1000px">
-            <div class="row">
-                <div class="col-12 px-0">
-                   
+                                <div class="col-8 px-1">
+                                    <div class="card shadow mb-4">
+                                                <div class="card-header py-3">
+                                                    <h6 class="m-0 font-weight-bold text-primary">Data Terbaru</h6>
+                                                </div>
+                                                <div class="card-body">
+                                
+                                                
+                                                <div class="container-bigger bg-white">
+                                                    <div class="row">
+                                                        <div class="col-md-6">
+                                                                <div class="card shadow mb-4">
+                                                                    <div class="card-header py-3">
+                                                                    <h6 class="m-0 font-weight-bold text-success">Kontrak Tender</h6>
+                                                                    </div>
+                                                                    <div class="card-body">
+                                                                        sdfsdfsdf
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+
+                                                        <div class="col-md-6">
+                                                            <div class="card shadow mb-4">
+                                                                <div class="card-header py-3">
+                                                                    <h6 class="m-0 font-weight-bold text-success">Kontrak Non Tender</h6>
+                                                                </div>
+                                                                <div class="card-body">
+                                                                    dfgdfgdfg
+                                                                </div>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-md-4">
+                                                        <div class="card shadow mb-4">
+                                                            <div class="card-header py-3">
+                                                                <h6 class="m-0 font-weight-bold text-success">Kontrak Berjalan</h6>
+                                                            </div>
+                                                            <div class="card-body">
+                                                                dfgdfgdfg
+                                                            </div>
+                                                        </div>
+                                                        </div>
+
+                                                        <div class="col-md-4">
+                                                        <div class="card shadow mb-4">
+                                                            <div class="card-header py-3">
+                                                                <h6 class="m-0 font-weight-bold text-success">Kontrak Selesai</h6>
+                                                            </div>
+                                                            <div class="card-body">
+                                                                dfgdfgdfg
+                                                            </div>
+                                                        </div>
+                                                        </div>
+
+                                                        <div class="col-md-4">
+                                                        <div class="card shadow mb-4">
+                                                            <div class="card-header py-3">
+                                                                <h6 class="m-0 font-weight-bold text-success">Kontrak Terbayar</h6>
+                                                            </div>
+                                                            <div class="card-body">
+                                                                dfgdfgdfg
+                                                            </div>
+                                                        </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                              
+                                                </div>
+                                    </div>
+                                </div>
+
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-6 px-0">
+                                <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+                            </div>
+                            <div class="col-6 px-0">
+                                
+                                <canvas id="myChart2" style="width:100%;max-width:600px"></canvas>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">                            
+                            <div class="col-12 px-1">
+
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                            <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
+                                            </tr>
+                                            </thead>
+                                            <tfoot>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
+                                            </tr>
+                                            </tfoot>
+                                            <tbody>
+                                            <tr>
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011/04/25</td>
+                                                <td>$320,800</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Garrett Winters</td>
+                                                <td>Accountant</td>
+                                                <td>Tokyo</td>
+                                                <td>63</td>
+                                                <td>2011/07/25</td>
+                                                <td>$170,750</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Ashton Cox</td>
+                                                <td>Junior Technical Author</td>
+                                                <td>San Francisco</td>
+                                                <td>66</td>
+                                                <td>2009/01/12</td>
+                                                <td>$86,000</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Cedric Kelly</td>
+                                                <td>Senior Javascript Developer</td>
+                                                <td>Edinburgh</td>
+                                                <td>22</td>
+                                                <td>2012/03/29</td>
+                                                <td>$433,060</td>
+                                            </tr>
+                                            
+                                            <tr>
+                                                <td>Donna Snider</td>
+                                                <td>Customer Support</td>
+                                                <td>New York</td>
+                                                <td>27</td>
+                                                <td>2011/01/25</td>
+                                                <td>$112,000</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                            </div>
+                        </div>
+                        <hr>
                 </div>
             </div>
 
-            </div>
 
-
-            </div>
             <div class="container container-bigger bg-white">
                 <div class="row">
                     <div class="col-12 px-0">
@@ -91,7 +305,64 @@
                         data-dismiss="modal">Close</button> </div>
             </div>
         </div>
-    <!-- </div>
+    </div>
+    
 </body>
+    <script src="admin/js/sb-admin-2.min.js"></script>
+  <!-- Page level plugins -->
+  <script src="admin/vendor/jquery/jquery.min.js"></script>
+  <script src="admin/vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  <script src="admin/js/demo/datatables-demo.js"></script>
+<script type="text/javascript">
+
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+        var yValues = [55, 49, 44, 24, 15];
+        var barColors = [
+        "#b91d47",
+        "#00aba9",
+        "#2b5797",
+        "#e8c3b9",
+        "#1e7145"
+        ];
+
+        new Chart("myChart", {
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            title: {
+            display: true,
+            text: "World Wide Wine Production 2018"
+            }
+        }
+        });
+
+        new Chart("myChart2", {
+        type: "bar",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            legend: {display: false},
+            title: {
+            display: true,
+            text: "World Wide Wine Production 2018"
+            }
+        }
+        });
+
+
+</script>
+
 
 </html>
