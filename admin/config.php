7b4dbb29a6859796d5c9c62b5b96a70f37d70815
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-20 16:10:47 +0700

    aaaa

--- admin/config.php
+++ admin/config.php
@@ -1,4 +1,4 @@
 <?php
-    $koneksi = mysqli_connect("localhost","root","","econtract_apps");
-    //$koneksi = mysqli_connect("localhost","smartkam_root","Barlok15@","smartkam_contract_apps");
+    //$koneksi = mysqli_connect("localhost","root","","econtract_apps");
+    $koneksi = mysqli_connect("localhost","smartkam_root","Barlok15@","smartkam_contract_apps");
 ?>
